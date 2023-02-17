b0227e75cf265ead24de1c63f0cddc11215a7669
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-02-17 17:35:25 +0200

    names added to userQuestions array

--- utils/questions.js
+++ utils/questions.js
@@ -1,39 +1,48 @@
 const userQuestions = [
         {
             type: 'input',
-            Message: "What is the title of your project?"
+            name: 'Project Title',
+            Message: 'What is the title of your project?'
         },
         {
             type: 'input',
-            Message:"What is your GitHub username?"
+            name: 'GitHub username',
+            Message:'What is your GitHub username?'
         },
         {
             type: 'input',
-            Message:"What email address would you like to use for this README?"
+            name: 'Email address',
+            Message:'What email address would you like to use for this README?'
         },
         {
             type: 'input',
-            Message:"What is the short description of this project?"
+            name: 'Short description',
+            Message:'What is the short description of this project?'
         },
         {
             type: 'input',
-            Message:"What are the steps required to install your project?"
+            name: 'Installation',
+            Message:'What are the steps required to install your project?'
         },
         {
             type: 'input',
-            Message:"How would a user use this project?"
+            name: 'Usage',
+            Message:'How would a user use this project?'
         },
         {
             type: 'list',
-            Message:"What license is this project under?"
+            name: 'License',
+            Message:'What license is this project under?'
         },
         {
             type: 'input',
-            Message:"How would a user contribute to this project?"
+            name: 'Contributing',
+            Message:'How would a user contribute to this project?'
         },
         {
             type: 'input',
-            Message:"What tests are there for this project?"
+            name: 'Tests',
+            Message:'What tests are there for this project?'
         },
     ];
 
