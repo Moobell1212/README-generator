993690a780cc1a0fc10dcc2664fb17872bcb7569
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-02-17 17:15:28 +0200

    seperate questions file added

--- index.js
+++ index.js
@@ -4,24 +4,37 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = ["What is the title of your project?",
-"What is your GitHub username?",
-"What email address would you like to use for this README?",
-"What is the short description of this project?",
-"What are the steps required to install your project?",
-"How would a user use this project?",
-"What license is this project under?",
-"How would a user contribute to this project?",
-"What tests are there for this project?",
-"What license is this project under?"
-];
+const questions = () =>
+    inquirer.prompt([
+        { 
+            type:"What is the title of your project?"},
+        { 
+            type:"What is your GitHub username?"},
+        { 
+            type:"What email address would you like to use for this README?"},
+        { 
+            type:"What is the short description of this project?"},
+        { 
+            type:"What are the steps required to install your project?"},
+        { 
+            type:"How would a user use this project?"},
+        { 
+            type:"What license is this project under?"},
+        { 
+            type:"How would a user contribute to this project?"},
+        { 
+            type:"What tests are there for this project?"},
+        { 
+            type:"What license is this project under?"}
+    ]);
 
 // function to write README file
 var writeToFile = (fileName, data) => {
 }
 
-function askQuestions () {
-   console.log("Hello!") 
+function askQuestions() {
+
+    console.log("Hello!");
 }
 
 // function to initialize program
