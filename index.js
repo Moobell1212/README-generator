2e089b57225c174992edb85844beb9fc92ccb886
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-02-17 16:44:11 +0200

    initial questions added to index.js

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
+const questions = ["What is the title of your project?",
+"What is your GitHub username?",
+"What email address would you like to use for this README?",
+"What is the short description of this project?",
+"What are the steps required to install your project?",
+"How would a user use this project?",
+"What license is this project under?",
+"How would a user contribute to this project?",
+"What tests are there for this project?",
+"What license is this project under?"
 ];
 
 // function to write README file
